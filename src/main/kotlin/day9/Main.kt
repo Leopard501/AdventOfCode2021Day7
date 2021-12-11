4c98222c86f912c276b379967f9c451b3da0c2cf
blakebabb <leopard50x1@gmail.com>
2021-12-11 13:32:40 -0800

    part 1

--- src/main/kotlin/day9/Main.kt
+++ src/main/kotlin/day9/Main.kt
@@ -1,9 +1,38 @@
 package day9
 
 import java.io.File
+import kotlin.collections.ArrayList
 
-val input = File("src/main/kotlin/day9/input").readLines()
+val input = File("src/main/kotlin/day9/test").readLines()
 
 fun main() {
+    val riskLevels = ArrayList<Array<Int>>()
+    for (y in input.indices) {
+        for (x in input[0].indices) {
+            val center = getVal(y, x)
+            if (getVal(y, x - 1) <= center) continue
+            if (getVal(y - 1, x) <= center) continue
+            if (getVal(y, x + 1) <= center) continue
+            if (getVal(y + 1, x) <= center) continue
+            riskLevels.add(arrayOf(y, x))
+        }
+    }
 
+    printVectorArray(riskLevels)
+}
+
+fun getVal(y: Int, x: Int): Int {
+    return try {
+        input[y][x].digitToInt()
+    } catch (ex: IndexOutOfBoundsException) {
+        10
+    }
+}
+
+fun printVectorArray(arr: ArrayList<Array<Int>>) {
+    print("[")
+    for (v in arr) {
+        print("(${v[1]}, ${v[0]}),")
+    }
+    print("]")
 }
